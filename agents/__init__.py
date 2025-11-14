822e777be85ae3aecc9fa55d7dc3f5af19fbcd0a
Anshul Dani <158159264+anshuldani@users.noreply.github.com>
2025-11-14 05:09:20 -0600

    Add files via upload

---
+++ agents/__init__.py
@@ -0,0 +1,10 @@
+# Agents Package
+from .analyst_agent import FinancialAnalystAgent
+from .governance_agent import GovernanceAnalystAgent
+from .thesis_generator import ThesisGeneratorAgent
+
+__all__ = [
+    'FinancialAnalystAgent',
+    'GovernanceAnalystAgent',
+    'ThesisGeneratorAgent'
+]
